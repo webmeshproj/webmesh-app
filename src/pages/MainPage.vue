34ff02e733a77db1860013c81fe6a53871f74319
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-03 17:02:13 +0200

    tidy and replace logos

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,14 +4,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    children: [{ path: '', component: () => import('pages/MainPage.vue') }],
   },
 ];
 
