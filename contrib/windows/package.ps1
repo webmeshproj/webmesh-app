87452a7e3eaa29f05f3ea7c75141264e9f4c7072
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-05 22:41:24 +0200

    ci: add draft wix packager

--- contrib/windows/package.ps1
+++ contrib/windows/package.ps1
@@ -1,3 +1,37 @@
 #!powershell
 
 # This script is used to build the Windows installer.
+
+Set-PSDebug -Trace 1
+$ErrorActionPreference = "Stop"
+
+$version=$env:VERSION
+if ($version -eq "") {
+    $version = "0.0.1"
+}
+
+$targetArch=$env:TARGET_ARCH
+if ($targetArch -eq "") {
+    $targetArch = "x64"
+}
+
+$daemonRoot=$env:DAEMON_ROOT
+if ($daemonRoot -eq "") {
+    $daemonRoot = "webmesh\dist"
+}
+if ($targetArch -eq "x64") {
+    $daemonPath = "$daemonRoot\webmeshd_windows_amd64_v1\webmeshd.exe"
+} else {
+    $daemonPath = "$daemonRoot\webmeshd_windows_arm64\webmeshd.exe"
+}
+
+New-Item -Path build -Type Directory
+
+Move-Item -Force "dist\electron\Packaged\Webmesh-0.0.1-$targetArch.exe" "build\Webmesh.exe"
+Move-Item -Force "$daemonPath" "build\webmeshd.exe"
+Copy-Item -Force "src-electron/icons/icon.png" "build\Webmesh.ico"
+
+Push-Location build
+candle "..\contrib\windows\webmesh.wxs"
+light -o "webmesh-$version.msi" "webmesh.wixobj"
+Pop-Location
