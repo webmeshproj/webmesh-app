abc5fba6546163731b0c86b7148b11b86a2b8a09
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-05 23:01:52 +0200

    scaffold darwin packaging scripts

--- contrib/darwin/package.sh
+++ contrib/darwin/package.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env bash
 
 # This script is used to build the macOS installer.
+
+rm -rf build
+mkdir -p build
+
+pkgbuild \
+		--root contrib/darwin/daemon \
+		--install-location "/Library/LaunchDaemons" \
+		--identifier com.webmeshproj.webmeshd \
+		--ownership recommended \
+		--scripts contrib/darwin/scripts \
+		build/daemon-scripts.pkg
+
+pkgbuild \
+		--root dist/electron/Packaged/mac \
+		--install-location "/Applications" \
+		--component-plist contrib/darwin/components.plist \
+		--identifier com.webmeshproj.app \
+		build/app-x64.pkg
+
+pkgbuild \
+		--root dist/electron/Packaged/mac-arm64 \
+		--install-location "/Applications" \
+		--component-plist contrib/darwin/components.plist \
+		--identifier com.webmeshproj.app \
+		build/app-arm64.pkg
+
+productbuild \
+    --package build/daemon-scripts.pkg \
+    --package build/app-x64.pkg \
+    build/Webmesh-x64-Unsigned.pkg
+
+productbuild \
+    --package build/daemon-scripts.pkg \
+    --package build/app-arm64.pkg \
+    build/Webmesh-arm64-Unsigned.pkg
--- contrib/windows/package.ps1
+++ contrib/windows/package.ps1
@@ -19,20 +19,32 @@ $daemonRoot=$env:DAEMON_ROOT
 if ($daemonRoot -eq "") {
     $daemonRoot = "webmesh\dist"
 }
-if ($targetArch -eq "x64") {
-    $daemonPath = "$daemonRoot\webmeshd_windows_amd64_v1\webmeshd.exe"
-} else {
-    $daemonPath = "$daemonRoot\webmeshd_windows_arm64\webmeshd.exe"
-}
 
-New-Item -Path build -Type Directory
+$daemonX64Path = "$daemonRoot\webmeshd_windows_amd64_v1\webmeshd.exe"
+$daemonARM64Path = "$daemonRoot\webmeshd_windows_arm64\webmeshd.exe"
 
-Write-Output "$version" | Out-File -Force -FilePath "build\version.txt"
-Move-Item -Force "dist\electron\Packaged\Webmesh-$version-$targetArch.exe" "build\Webmesh.exe"
-Move-Item -Force "$daemonPath" "build\webmeshd.exe"
-Copy-Item -Force "src-electron/icons/icon.png" "build\Webmesh.ico"
+New-Item -Path "build" -Type Directory
+New-Item -Path "build\x64" -Type Directory
+New-Item -Path "build\arm64" -Type Directory
 
-Push-Location build
+Write-Output "$version" | Out-File -Force -FilePath "build\x64\version.txt"
+Write-Output "$version" | Out-File -Force -FilePath "build\arm64\version.txt"
+
+Move-Item -Force "dist\electron\Packaged\Webmesh-$version-x64.exe" "build\x64\Webmesh.exe"
+Move-Item -Force "dist\electron\Packaged\Webmesh-$version-arm64.exe" "build\arm64\Webmesh.exe"
+
+Move-Item -Force "$daemonX64Path" "build\x64\webmeshd.exe"
+Move-Item -Force "$daemonARM64Path" "build\arm64\webmeshd.exe"
+
+Copy-Item -Force "src-electron/icons/icon.png" "build\x64\Webmesh.ico"
+Copy-Item -Force "src-electron/icons/icon.png" "build\arm64\Webmesh.ico"
+
+Push-Location build\x64
 candle "..\contrib\windows\webmesh.wxs"
-light -o "webmesh-$version.msi" "webmesh.wixobj"
+light -o "webmesh-x64-$version.msi" "webmesh.wixobj"
+Pop-Location
+
+Push-Location build\arm64
+candle "..\contrib\windows\webmesh.wxs"
+light -o "webmesh-arm64-$version.msi" "webmesh.wixobj"
 Pop-Location
