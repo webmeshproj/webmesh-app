6c4aa28bc5d5a7822b5c8d0837749fb5c24c92cc
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-06 15:12:15 +0200

    win: more packaging tidy

--- contrib/windows/package.ps1
+++ contrib/windows/package.ps1
@@ -10,42 +10,48 @@
 Set-PSDebug -Trace 1
 $ErrorActionPreference = "Stop"
 
-$version=$env:VERSION
+$VERSION=$env:VERSION
 if (-not $version) {
     $version = "0.0.1"
 }
 
-$daemonRoot=$env:DAEMONROOT
+$DAEMONROOT=$env:DAEMONROOT
 if (-not $daemonRoot) {
-    $daemonRoot = "webmesh"
+    $DAEMONROOT = "webmesh"
 }
 
-$daemonX64Path = "$daemonRoot\dist\webmeshd_windows_amd64_v1\webmeshd.exe"
-$daemonARM64Path = "$daemonRoot\dist\webmeshd_windows_arm64\webmeshd.exe"
+$DAEMONX64PATH = "$DAEMONROOT\dist\webmeshd_windows_amd64_v1\webmeshd.exe"
+$DAEMONARM64PATH = "$DAEMONROOT\dist\webmeshd_windows_arm64\webmeshd.exe"
 
 Remove-Item -Force -Recurse -ErrorAction Ignore "build"
 New-Item -Path "build" -Type Directory
 New-Item -Path "build\x64" -Type Directory
 New-Item -Path "build\arm64" -Type Directory
 
-Write-Output "$version" | Out-File -Force -FilePath "build\x64\version.txt"
-Write-Output "$version" | Out-File -Force -FilePath "build\arm64\version.txt"
+Write-Output "$VERSION" | Out-File -Force -FilePath "build\x64\version.txt"
+Write-Output "$VERSION" | Out-File -Force -FilePath "build\arm64\version.txt"
 
-Copy-Item -Force "dist\electron\Packaged\Webmesh-$version-x64.exe" "build\x64\Webmesh.exe"
-Copy-Item -Force "dist\electron\Packaged\Webmesh-$version-arm64.exe" "build\arm64\Webmesh.exe"
+Copy-Item "LICENSE" "build\x64\LICENSE.txt"
+Copy-Item "LICENSE" "build\arm64\LICENSE.txt"
 
-Copy-Item -Force "$daemonX64Path" "build\x64\webmeshd.exe"
-Copy-Item -Force "$daemonARM64Path" "build\arm64\webmeshd.exe"
+Copy-Item -Force "dist\electron\Packaged\Webmesh-$VERSION-x64.exe" "build\x64\Webmesh.exe"
+Copy-Item -Force "dist\electron\Packaged\Webmesh-$VERSION-arm64.exe" "build\arm64\Webmesh.exe"
+
+Copy-Item -Force "$DAEMONX64PATH" "build\x64\webmeshd.exe"
+Copy-Item -Force "$DAEMONARM64PATH" "build\arm64\webmeshd.exe"
 
 Copy-Item -Force "src-electron/icons/icon.png" "build\x64\Webmesh.ico"
 Copy-Item -Force "src-electron/icons/icon.png" "build\arm64\Webmesh.ico"
 
+sed "s/ProductVersion = `".*`"/ProductVersion = `"$VERSION`"/" "contrib/windows/webmesh.wxs" \
+  | Set-Content "contrib\windows\webmesh.wxs"
+
 Push-Location build\x64
 candle "..\..\contrib\windows\webmesh.wxs"
-light -o "webmesh-x64-$version.msi" "webmesh.wixobj"
+light -o "webmesh-x64-$VERSION.msi" "webmesh.wixobj"
 Pop-Location
 
 Push-Location build\arm64
 candle "..\..\contrib\windows\webmesh.wxs"
-light -o "webmesh-arm64-$version.msi" "webmesh.wixobj"
+light -o "webmesh-arm64-$VERSION.msi" "webmesh.wixobj"
 Pop-Location
